995dd44c4723b269c41e32dd014a6e1c71611116
nekoniumoretai <152590285+nekoniumoretai@users.noreply.github.com>
2024-01-16 05:38:55 +0000

    Finish task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,12 +26,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
        flash[:notice] = "Update was completed successfully"
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      @book = Book.find(params[:id])
+      
       flash.now[:notice]
       render :edit
     end
